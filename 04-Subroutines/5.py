8b8994aa635885049b641a0222c1a33217b3fc3b
Kamil Stefański <94528279+banditt37@users.noreply.github.com>
2022-12-06 10:40:48 +0100

    Update 5.py

--- 04-Subroutines/5.py
+++ 04-Subroutines/5.py
@@ -1,10 +1,5 @@
 def display_university_address():
-    print("Cracow University of Economics")
-    print("Rakowicka 27")
-    print("31-510 Kraków, POLAND")
+    print(f"Cracow University of Economics\nRakowicka 27\n31-510 Kraków, POLAND\n")
 
-print()
 display_university_address()
-print()
 display_university_address()
-print()
