4e74db6913eb6ef5b61632a7e6b02fdefd4ca3bb
Kamil Stefański <94528279+banditt37@users.noreply.github.com>
2022-12-08 11:13:05 +0100

    Update 6.py

--- 06-Arrays/6.py
+++ 06-Arrays/6.py
@@ -1,15 +1,30 @@
 arr = [2, 3, 7, 5, 4]
+
+#a
 print(arr)
+#b
 print(len(arr))
+#c
 print(arr[0])
+#d
 print(arr[1])
-print(arr[len(arr) -1])
+print()
+#e
+print(arr[len(arr)-1])
+print()
+#f
 print(arr[len(arr)-2])
+print()
+#g
 print(arr[len(arr)//2])
+print()
+#h
 print(arr[int(len(arr)/2)])
-
+print()
+#i
 for i in arr:
-    print(i, end=" ")
-
-for i in range (len(arr)):
+    print(i, end=' ')
+#j
+print()
+for i in range (len(arr)//2):
     print(arr[i], end=" ")
