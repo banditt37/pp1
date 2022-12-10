cab6c74c3c19f7b2337f6e561f58a788144fbdd6
Kamil Stefański <94528279+banditt37@users.noreply.github.com>
2022-12-10 12:06:44 +0100

    update

---
+++ 08-DictionariesStacksAndQueues/10.py
@@ -0,0 +1,11 @@
+countries = [
+    {"France": 67236432},
+    {"France": 67236432},
+    {"France": 67236432},
+    {"France": 67236432},
+    {"France": 67236432},
+]
+
+for c in countries:
+    for k, v in c.items():
+        print(f"{k}: {v}")
---
+++ 08-DictionariesStacksAndQueues/9.py
@@ -0,0 +1,8 @@
+phone = {
+    "brand": "Samsung",
+    "model": "S21 FE",
+    "year": 2022,
+    "color": "black",
+}
+for k, v in phone.items():
+    print(k,":",v)
