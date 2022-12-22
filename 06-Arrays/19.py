196333c717a924b55bf6f146e7e2fb79d1d773e7
banditt37 <kamilstefanski00@gmail.com>
2022-12-22 13:18:05 +0100

    commit

---
+++ 06-Arrays/19.py
@@ -0,0 +1,15 @@
+def star():
+    arr = [12, 6, 4, 9, 10]
+    x = str()
+    i = 0
+    for i in arr:
+        for i in range(0, i, 1):
+            x += '*'
+        
+        print(f'{len(x)}: {x}')
+        x = ''
+
+star()
+
+
+
