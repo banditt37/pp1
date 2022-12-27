7b6a67d7ad79c00e305cf576ca5d6b54af9d6097
banditt37 <kamilstefanski00@gmail.com>
2022-12-27 17:23:31 +0100

    commit

---
+++ 06-Arrays/21.py
@@ -0,0 +1,7 @@
+def compare(array1, array2):
+    print(array1 == array2)
+
+compare(["water","book","sky"],["water","book","sky"])
+compare([True,False],[True,False,True])
+compare([5,3,1],[5,3,1])
+compare([3,2,1],[3,2])
