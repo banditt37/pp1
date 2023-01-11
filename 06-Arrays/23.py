058b94dcaf483f10b19ac9fb62bf650bb161d2a2
banditt37 <kamilstefanski00@gmail.com>
2023-01-11 14:11:16 +0100

    commit

--- 06-Arrays/23.py
+++ 06-Arrays/23.py
@@ -2,14 +2,13 @@ array2 = [5,1,36]
 array3 = [1, 3, 5, 6, 4, 2, 8, 7, 10, 9]
 
 def bubblesort(array):
-    print(array)
-    for i in range(0, len(array)-1):
-        for j in range(0, len(array) - i - 1):
+    print("unsorted array:", array)
+    n = len(array)
+    for i in range(0, n-1):
+        for j in range(0, n-1-i):
             if array[j] > array[j+1]:
-                temp = array[j]
-                array[j] = array[j+1]
-                array[j+1] = temp
-    print(array)
+                array[j], array[j+1] = array[j+1], array[j]
+    print("sorted array:", array)
 
 
 bubblesort([4,36,12,28,9,44,5])
--- 06-Arrays/26.py
+++ 06-Arrays/26.py
@@ -1,1 +1,13 @@
-array=[5,1,9,6,1]
+#26.	Write a program to find the second largest element in an array. Sample result:
+#Array: [5,1,9,6,1]
+#Result: 6
+
+
+array = [5,1,9,6,1]
+largest = []
+x = 0
+for i in array:
+    if x < i:
+        largest.append(i)
+        x = i
+print(largest[-2])
