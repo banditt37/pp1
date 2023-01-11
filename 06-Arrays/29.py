0ef4c5a28b4d86177d07d63c1eac84426b5ce205
banditt37 <kamilstefanski00@gmail.com>
2023-01-11 16:15:53 +0100

    commit

--- 06-Arrays/29.py
+++ 06-Arrays/29.py
@@ -1,11 +1,11 @@
 #29.	Write a program that, for the given array of real numbers, displays the number of elements that are greater than the given value entered from the keyboard.
-array = []
-count = 0
-k = int(input("Enter the real number: "))
-n = int(input("enter the number of elements in array: "))
-for i in range(0, n):
-    array.append(int(input()))
-for i in array:
-    if i > k:
-        count+=1
-print("The number of elements that are greater than the given value is", count)
+def f(k, n):
+    array = []
+    count = 0
+    for i in range(0, n):
+        array.append(int(input()))
+    for i in array:
+        if i > k:
+            count+=1
+    print("The number of elements that are greater than the given value is", count)
+f(int(input("Enter the real number: ")), int(input("enter the number of elements in array: ")))
---
+++ 06-Arrays/30.py
@@ -0,0 +1,6 @@
+def minmax(array):
+    array = []
+    n = int(input("enter the number of elements in array: "))
+    for i in range(0, n):
+        array.append(int(input()))
+
