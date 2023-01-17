62fc61356189b2130087da8e4658ea5a6448398a
banditt37 <kamilstefanski00@gmail.com>
2023-01-17 15:06:18 +0100

    c

---
+++ 08-DictionariesStacksAndQueues/15.py
@@ -0,0 +1,34 @@
+alphabet = {
+  "A": "Alfa",
+  "B": "Bravo",
+  "C": "Charlie",
+  "D": "Delta",
+  "E": "Echo",
+  "F": "Foxtrot",
+  "G": "Golf",
+  "H": "Hotel",
+  "I": "India",
+  "J": "Juliett",
+  "K": "Kilo",
+  "L": "Lima",
+  "M": "Mike",
+  "N": "November",
+  "O": "Oscar",
+  "P": "Papa",
+  "Q": "Quebec",
+  "R": "Romeo",
+  "S": "Sierra",
+  "T": "Tango",
+  "U": "Uniform",
+  "V": "Victor",
+  "W": "Whiskey",
+  "X": "X-ray",
+  "Y": "Yankee",
+  "Z": "Zulu"
+}
+
+file = open("icao.txt", "wt")
+
+for key, value in alphabet.items():
+    file.write(key+':'+value+'\n')
+file.close()
